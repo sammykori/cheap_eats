8ac2122ac3753ac92628265e9b35aae92e248920
Gh0stM0de <=jdotcole233@gmail.com>
2020-08-16 21:26:57 +0000

    food test

--- app/Http/Controllers/UssdController.php
+++ app/Http/Controllers/UssdController.php
@@ -41,6 +41,7 @@ class UssdController extends Controller
         $session_id = base64_encode($phone_number);
         if (strcmp($customer_interaction, "User timeout") == 0)
         {
+            Log::info("I am here at time out");
             Redis::del('select:'.$session_id);
             Redis::del($session_id);
         }
